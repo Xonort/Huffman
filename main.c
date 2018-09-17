f1e3bdf0ecdf7169a45a2d8cac045036f230544d
Xonort <43341922+Xonort@users.noreply.github.com>
2018-09-17 14:19:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+typedef struct noeud* nd;
+struct noeud{
+    nd gauche;
+    void* valeur;
+    nd droite;
+};
+
+typedef struct arbre* arbre;
+struct arbre{
+    nd tete;
+};
+
+arbre creer_arbre(){
+    arbre a = (arbre)malloc(sizeof(arbre));
+    return a;
+}
+
+void switch_val(){
+
+}
+
+void add_nod(arbre a , nd nod){
+    nd it=a->tete;
+
+
+}
+
+int main()
+{
+
+    printf("Hello world!\n");
     return 0;
 }
